4041d30614b449360b4baf7f529e40e754a8c95f
Stan Saal <stanislav.saal@ya.ru>
2023-02-20 17:52:14 +0300

    Change folders structure

--- problems/missingNumber/JS/binarySearch.test.js
+++ problems/missingNumber/JS/binarySearch.test.js
@@ -1,6 +1,6 @@
-const missingNumber = require("./missingNumber");
+const missingNumber = require("./binarySearch");
 
-describe("Missing number function", () => {
+describe("Missing number function by Binary Search", () => {
   it("should find the only number that missing in array [0...n], n = array.length", () => {
     expect(missingNumber([1, 2])).toEqual(0);
     expect(missingNumber([3, 0, 1])).toEqual(2);
